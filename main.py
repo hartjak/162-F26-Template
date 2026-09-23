0c95b5c6c6e1474a6a9e993ed401370c6a55ce04
Jake Hart <hartjak@mail.gvsu.edu>
2026-09-23 15:06:08 +0000

    Added score and asked for password.

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+'''
+password_strength.py: A module to check the strength of a password provided
+by a user.
+This should not be used for actual passwords; instead it is simply a coding
+exercise.
+Jake Hart
+CIS 162 - SECTION YOUR SECTION
+09/21/26 
+'''
+score = int(0)
+print("My Password Strength Checker")
+print("       By Jake Hart")
+print("")
+password = input(" Enter your password: ")
+
+
+
 
 # Data for plotting
 t = np.arange(0.0, 2.0, 0.01)
